f3b713efb7f788126e748be7ffadf27ae40b4b04
gundamboy <rowland.charles@gmail.com>
2018-11-04 21:51:08 -0700

    added database class

---
+++ app/src/main/java/com/charlesrowland/popularmovies/data/FavoriteMovieDatabase.java
@@ -0,0 +1,23 @@
+package com.charlesrowland.popularmovies.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = FavoriteMovie.class, version = 1)
+public abstract class FavoriteMovieDatabase extends RoomDatabase {
+
+    private static FavoriteMovieDatabase instance;
+
+    public abstract FavoriteMovieDao favoriteMovieDao();
+
+    public static synchronized FavoriteMovieDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), FavoriteMovieDatabase.class,
+                    "favorite_movie_database").fallbackToDestructiveMigration().build();
+        }
+
+        return instance;
+    }
+}
